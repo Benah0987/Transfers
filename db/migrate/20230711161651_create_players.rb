6b5d46440e970c9e2aeca624c87cb7bc2015896e
Benah0987 <wanyoikebenayah77@gmail.com>
2023-07-11 19:17:41 +0300

    creating a  rails app

---
+++ db/migrate/20230711161651_create_players.rb
@@ -0,0 +1,13 @@
+class CreatePlayers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :players do |t|
+      t.string :name
+      t.integer :age
+      t.integer :stamina
+      t.integer :physical
+      t.integer :pace
+
+      t.timestamps
+    end
+  end
+end
